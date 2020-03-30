dda330ef56bf3936074adddecd0c14ccc2206282
MAHFOUDH KERBOUCHE <kerbouche.mahfoudh@gmail.com>
2020-03-30 11:38:22 +0100

    made a few changes on js code

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,21 @@
-url = 'http://127.0.0.1:8000/update/'
+
+
+function main(){
+	ajaxCall();
+  	repeat = setInterval(ajaxCall,30000);
+
+  }
+$(document).ready(main);
+
+$(function() {
+
+          $.scrollify({
+            section : ".section",
+          });
+        });
+
+function ajaxCall(){
+	url = 'http://127.0.0.1:8000/update/'
 total_dz = $('#total-dz');
 total_world = $('#total-world') ;
 active_dz = $('#active-dz');
@@ -7,24 +24,12 @@ recovered_dz = $('#recover-dz');
 recovered_world = $('#recover-world');
 death_dz = $('#death-dz');
 death_world = $('#death-world');
-$(function() {
-          $.scrollify({
-            section : ".section",
-          });
-        });
-  	
-function main(){
-	ajaxCall();
-  	repeat = setInterval(ajaxCall,30000);
-  }
-$(document).ready(main);
-
-
-function ajaxCall(){
+	
   	$.ajax({
   		method: 'GET',
   		url: url,
   		success: function(data){
+  		
   			total_dz.text(data['column_1']).hide().fadeIn(1000); 
   			total_world.text(data["Coronavirus Cases"]).hide().fadeIn(1000);
   			active_dz.text(data['column_6']).hide().fadeIn(1000);
@@ -33,11 +38,13 @@ function ajaxCall(){
   			recovered_world.text(data['Recovered']).hide().fadeIn(1000);
   			death_dz.text(data['column_3']).hide().fadeIn(1000);
   			death_world.text(data['Deaths']).hide().fadeIn(1000);
+  		
   		},
   		error: function(error_data){
-  			alert('error')
+  			console.log(error_data)
   		}
   	})
+
 
   	/*var http = new XMLHttpRequest();
   	http.open('GET',url);
