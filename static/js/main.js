c858051ebc21d13940edb591b67e64b8277d8975
FadyBengoufa <fady.bengoufa@gmail.com>
2020-03-30 14:46:22 +0100

    added split screen prevent section

--- static/js/main.js
+++ static/js/main.js
@@ -16,14 +16,14 @@ $(function() {
 
 function ajaxCall(){
 	url = 'http://127.0.0.1:8000/update/'
-total_dz = $('#total-dz');
-total_world = $('#total-world') ;
-active_dz = $('#active-dz');
-active_world = $('#active-world');
-recovered_dz = $('#recover-dz');
-recovered_world = $('#recover-world');
-death_dz = $('#death-dz');
-death_world = $('#death-world');
+	total_dz = $('#total-dz');
+	total_world = $('#total-world') ;
+	active_dz = $('#active-dz');
+	active_world = $('#active-world');
+	recovered_dz = $('#recover-dz');
+	recovered_world = $('#recover-world');
+	death_dz = $('#death-dz');
+	death_world = $('#death-world');
 	
   	$.ajax({
   		method: 'GET',
@@ -53,3 +53,80 @@ death_world = $('#death-world');
   	}
   	http.send();*/
   }
+
+/**
+ * Prevention Section
+ */
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+
+/**
+ * Section One
+ */
+one.addEventListener("mouseenter", () => {
+	one.classList.add("maximize");
+	two.classList.add("minimize");
+	three.classList.add("minimize");
+	four.classList.add("minimize");
+});
+
+one.addEventListener("mouseleave", () => {
+	one.classList.remove("maximize");
+	two.classList.remove("minimize");
+	three.classList.remove("minimize");
+	four.classList.remove("minimize");
+});
+
+/**
+ * Section Two
+ */
+two.addEventListener("mouseenter", () => {
+	two.classList.add("maximize");
+	one.classList.add("minimize");
+	three.classList.add("minimize");
+	four.classList.add("minimize");
+});
+
+two.addEventListener("mouseleave", () => {
+	two.classList.remove("maximize");
+	one.classList.remove("minimize");
+	three.classList.remove("minimize");
+	four.classList.remove("minimize");
+});
+
+/**
+ * Section Three
+ */
+three.addEventListener("mouseenter", () => {
+	three.classList.add("maximize");
+	one.classList.add("minimize");
+	two.classList.add("minimize");
+	four.classList.add("minimize");
+});
+
+three.addEventListener("mouseleave", () => {
+	three.classList.remove("maximize");
+	one.classList.remove("minimize");
+	two.classList.remove("minimize");
+	four.classList.remove("minimize");
+});
+ /**
+ * Section Four
+ */
+
+four.addEventListener("mouseenter", () => {
+	four.classList.add("maximize");
+	one.classList.add("minimize");
+	two.classList.add("minimize");
+	three.classList.add("minimize");
+});
+
+four.addEventListener("mouseleave", () => {
+	four.classList.remove("maximize");
+	one.classList.remove("minimize");
+	two.classList.remove("minimize");
+	three.classList.remove("minimize");
+});
